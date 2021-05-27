cd30a4cb113ba0d1fc0003518c8315fd3d6b21ee
VMoiseev <e175@inbox.ru>
2021-05-27 11:23:00 +0500

    ДЗ выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,23 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+
+  let D = b ** 2 - 4 * a * c;
+
+  if (D < 0) {
+    return [];
+  } else if (D === 0) {
+    return [(- b + Math.sqrt(D)) / 2 * a];
+  } else if (D > 0) {
+    return [(- b + Math.sqrt(D)) / 2 * a, (- b - Math.sqrt(D)) / 2 * a];
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  let loanBody = +amount - Number(contribution);
+  let creditPeriod = (date.getFullYear() - new Date().getFullYear()) * 12 + date.getMonth() - new Date().getMonth();
+  let monthPercent = Number(percent) / 12 / 100;
+  let paymentPerMonth = loanBody * (monthPercent + monthPercent / (((1 + monthPercent) ** creditPeriod) - 1));
+  let totalAmount = Number((paymentPerMonth * creditPeriod).toFixed(2));
   return totalAmount;
 }
