9af48d6a90c20e44e255db89d4fef01b4c88557e
VMoiseev <e175@inbox.ru>
2021-05-30 13:18:06 +0500

    Выполнено ДЗ

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,44 @@
 // Задание 1
 function getArrayParams(arr) {
   let min,max,sum,avg;
+  min = -101;
+  max = 101;
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = (sum / arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = 0;
 
-  // Ваш кода
-  // for ...
+  for (let i = 0; i < arrOfArr.length; i++) {
+    sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   
-  return max
+  return max;
 }
