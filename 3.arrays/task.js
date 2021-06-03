6a00ecaf592dd21872ba4cc16a91214eaf2a83d3
VMoiseev <e175@inbox.ru>
2021-06-03 18:31:42 +0500

    ДЗ сделано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,27 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
-
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
 }
 
-function advancedFilter(arr) {
-  let resultArr;
+// Удлинённое написание решения первой задачи
 
-  // Ваш код
+//function compareArrays(arr1, arr2) {
+  //let result;
+
+  //if (arr1.length === arr2.length && arr1.every(function(item, i) {
+    //return item === arr2[i];
+  //})) {
+    //result = true;
+  //} else {
+    //result = false;
+  //}
+
+  //return result;
+//}
+
+function advancedFilter(arr) {
+
+  const resultArr = arr.filter((number) => number > 0).filter((number) => number % 3 === 0).map((number) => number * 10);
 
   return resultArr; // array
 }
