3931f1bd5171bc4d0c0ec4234ce7f29dc19a4dde
VMoiseev <e175@inbox.ru>
2021-06-17 11:58:05 +0500

    ДЗ выполнено

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,12 +1,81 @@
+//Задача №1
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(",");
+    let idx = cache.findIndex((item) => item.hash === hash);
+    
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[idx].value);
+      return "Из кэша: " + cache[idx].value;
+    } 
+    
+    let result = func(...args);
+    cache.push({hash: hash, value: result});
+    if (cache.length > 5) {
+      cache.shift();
+    } 
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
+const addThree = (a, b, c) => a + b + c;
+const upgradedAddThree = cachingDecoratorNew(addThree);
+upgradedAddThree(1, 2, 3); // вычисляем: 6
+upgradedAddThree(1, 2, 3); // из кэша: 6
+upgradedAddThree(2, 2, 3); // вычисляем: 7
+upgradedAddThree(3, 2, 3); // вычисляем: 8
+upgradedAddThree(4, 2, 3); // вычисляем: 9
+upgradedAddThree(5, 2, 3); // вычисляем: 10
+upgradedAddThree(6, 2, 3); // вычисляем: 11   (при этом кэш для 1, 2, 3 уничтожается)
+upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+//Задача №2
+
+function debounceDecoratorNew(func, ms) {
+  let flag = false;
+
+  function wrapper(...args) {
+    if (!flag) {
+      func(...args);
+      flag = true;
+      setTimeout(() => {
+        flag = false;
+      } , ms);
+    } 
+  }
+  return wrapper;
 }
+
+const sendSignal = () => console.log("Сигнал отправлен");
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+setTimeout(upgradedSendSignal); // Сигнал отправлен
+setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс
+setTimeout(upgradedSendSignal, 900); // проигнорировано аналогично
+setTimeout(upgradedSendSignal, 1200); // проигнорировано аналогично
+setTimeout(upgradedSendSignal, 2300); // Сигнал отправлен
+setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен
+setTimeout(upgradedSendSignal, 4500); // Сигнал отправлен
+
+//Задача №3
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let flag = false;
+  wrapper.count = 0;
+
+  function wrapper(...args) {
+    if (!flag) {
+      wrapper.count++;
+      func(...args);
+      flag = true;
+      setTimeout(() => {
+        flag = false;
+      } , ms);
+    }
+  }
+  return wrapper;
 }
